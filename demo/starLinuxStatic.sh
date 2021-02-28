2319e935d51b9b691bc9edc40e5a5a9f8794ee3d
jligm-hash <>
2021-02-28 18:58:20 +0800

    add Linux Static

---
+++ demo/starLinuxStatic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ulimit -n 4096
+
+# star="./macOS/STAR"
+star="./linuxStaticOS/STAR"
+
+myGenomeDir="./data/genome/"
+myGenomeFastaFiles="./data/genome/chr21.hg37.fa"
+mySjdbGTFfile="./data/genome/chr21.hg37.gtf"
+myFastqFile1="./data/RNAseq/demo.chr21.1.fastq.gz"
+myFastqFile2="./data/RNAseq/demo.chr21.2.fastq.gz"
+myFileNamePrefix="./data/result/result."
+
+${star} \
+ --runThreadN 12 \
+ --genomeDir ${myGenomeDir} \
+ --sjdbGTFfile ${mySjdbGTFfile} \
+ --sjdbOverhang 100 \
+ --readFilesIn ${myFastqFile1} ${myFastqFile2} \
+ --readFilesCommand gunzip -c \
+ --outFileNamePrefix ${myFileNamePrefix} \
+ --outSAMtype BAM SortedByCoordinate
